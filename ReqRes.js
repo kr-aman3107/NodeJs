c1cf5754949254b713732e6b78559d7593d20118
Aman kumar <aman915588@gmail.com>
2024-08-06 23:09:55 +0530

    ReqRes commit

---
+++ ReqRes.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+
+const server = http.createServer((req,res) =>{
+    console.log("server is running on port " + 4000);
+    res.setHeader("Content-Type" , 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Frist Page</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>')
+});
+server.listen(4000);
