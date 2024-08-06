fc8dab1cd017db1fd25ed961875b5407293e295a
Aman kumar <aman915588@gmail.com>
2024-08-06 17:24:54 +0530

    server created on port 4000 and logging name

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const http = require('http');
 const server = http.createServer((req,res) =>{
     console.log("Aman");
 });
-server.listen(4000);
+server.listen(4000,()=>{
+    console.log("server is running on port " + 4000);
+});
