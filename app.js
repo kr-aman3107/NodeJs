595a48954a0b5f01c0cb362d2a1a987a4dc39bc4
Aman kumar <aman915588@gmail.com>
2024-08-13 13:40:18 +0530

    with one more input tag

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyPerser = require('body-parser');
+
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyPerser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next) => {
 
+    res.send('<form action="product" method="POST"><input type="text" name="title"><button type=""Submit>Add Product</button></form>');
+    res.send('<form action="product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type=""Submit>Add Product</button></form>');
+    next();
+});
 
-app.use('/add-product',(req,res,next) =>{
-    res.send('<form action="/product" method = "POST"><input type = "text" name= "title"><button type= "submit">Add Product</button></form>');   
-    
-})
-
-app.post('/product',(req,res,next) =>{
+app.use('/product',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/');
 })
-
-app.use('/',(req,res,next) =>{
-    res.send('<h1>Hello from nodejs</h1>');    
+app.use('/',(req,res,next) => {
+    console.log('In the second middleware!');
+    res.send('<h1>Hello from Nodejs</h1>');
 })
-
 app.listen(3000);
-
