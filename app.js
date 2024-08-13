8ad54d94cb69547687ab86fb4ed0ada73bdbd4f3
Aman kumar <aman915588@gmail.com>
2024-08-13 13:28:12 +0530

    created /add-product route

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res,next) =>{
-    console.log("In the Middleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/add-product',(req,res,next) =>{
+    res.send('<form action="/product" method = "POST"><input type = "text" name= "title"><button type= "submit">Add Product</button></form>');   
+    
 })
-app.use((req,res,next) =>{
-    console.log("In another Middleware");
+
+app.post('/product',(req,res,next) =>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next) =>{
     res.send('<h1>Hello from nodejs</h1>');    
 })
 
