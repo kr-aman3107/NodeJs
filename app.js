a69f7cff215d86ec9dfc257f8a49113ebe4a91c7
Aman kumar <aman915588@gmail.com>
2024-08-13 14:58:04 +0530

    Use routers to clean up the code

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-const http = require('http');
 const express = require('express');
-const bodyPerser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyPerser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next) => {
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-    res.send('<form action="product" method="POST"><input type="text" name="title"><button type=""Submit>Add Product</button></form>');
-    res.send('<form action="product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type=""Submit>Add Product</button></form>');
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res) =>{
+    res.status(404).send('<h1>Page Not Found</h2>')
 });
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-app.use('/',(req,res,next) => {
-    console.log('In the second middleware!');
-    res.send('<h1>Hello from Nodejs</h1>');
-})
 app.listen(3000);
